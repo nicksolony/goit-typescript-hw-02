951a0de1132c97f2fab950c7f447685ef680cf50
nicksolony <nicksolony@gmail.com>
2023-11-10 23:40:52 -0500

    finished generics

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,20 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface Component<T> {
+  props: T;
+  pageInfo(): void;
 }
 
-class Page extends Component {
+class Page<T> implements Component<T> {
+  props: T & {
+    title: string;
+  };
+
+  // constructor(props: T) {
+  //   this.props = props;
+  // };
+
   pageInfo () {
     console.log(this.props.title);
   }
